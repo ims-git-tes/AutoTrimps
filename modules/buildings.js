3ee788454fbe6c0b614802b6556b5915a0e7b1ca
ims-git-tes <85272598+ims-git-tes@users.noreply.github.com>
2021-06-03 06:50:42 +0200

    Update buildings.js
    put it limter for buying nurseries to avoid stalling warpstations on gems. Oly buys nurseries if gemcost is <10% of warkstation cost.

--- modules/buildings.js
+++ modules/buildings.js
@@ -202,7 +202,7 @@ function buyBuildings() {
         safeBuyBuilding('Tribute');
     }
     //Nurseries
-    if (game.buildings.Nursery.locked == 0 && (!hidebuild &&( game.global.world >= getPageSetting('NoNurseriesUntil') || getPageSetting('NoNurseriesUntil') < 1) && (getPageSetting('MaxNursery') > game.buildings.Nursery.owned || getPageSetting('MaxNursery') == -1)) || (game.global.challengeActive != "Daily" && getPageSetting('PreSpireNurseries') > game.buildings.Nursery.owned && isActiveSpireAT()) || (game.global.challengeActive == "Daily" && getPageSetting('dPreSpireNurseries') > game.buildings.Nursery.owned && disActiveSpireAT())) {
+    if (game.buildings.Nursery.locked == 0 && (getBuildingItemPrice(game.buildings.Nursery, "gems", false, 1) * 10 < getBuildingItemPrice(game.buildings.Warpstation, "gems", false, 1) ) && (!hidebuild &&( game.global.world >= getPageSetting('NoNurseriesUntil') || getPageSetting('NoNurseriesUntil') < 1) && (getPageSetting('MaxNursery') > game.buildings.Nursery.owned || getPageSetting('MaxNursery') == -1)) || (game.global.challengeActive != "Daily" && getPageSetting('PreSpireNurseries') > game.buildings.Nursery.owned && isActiveSpireAT()) || (game.global.challengeActive == "Daily" && getPageSetting('dPreSpireNurseries') > game.buildings.Nursery.owned && disActiveSpireAT())) {
 	safeBuyBuilding('Nursery');
     }
 
